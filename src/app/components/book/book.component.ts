3ba39ef4b3987d40e7f0578aa0f0349d4e00462b
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-12-18 10:37:38 +0530

    Add files via upload

---
+++ src/app/components/book/book.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+
+export interface Book {
+  id: number;
+  title: string;
+  author: string;
+  genre: string;
+  publicationYear: number;
+}
+
+@Component({
+  selector: 'app-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.css']
+})
+export class BookComponent {
+  books: Book[] = [];
+  newBook: Book = {} as Book;
+  editedBook: Book = {} as Book;
+  isEditing = false;
+  searchKeyword = '';
+
+  constructor() { }
+
+  addBook(): void {
+    this.newBook.id = this.books.length + 1;
+    this.books.push({ ...this.newBook });
+    this.newBook = {} as Book;
+  }
+
+  editBook(book: Book): void {
+    this.isEditing = true;
+    this.editedBook = { ...book };
+  }
+
+  saveEditedBook(): void {
+    const index = this.books.findIndex(book => book.id === this.editedBook.id);
+    if (index !== -1) {
+      this.books[index] = { ...this.editedBook };
+    }
+    this.cancelEdit();
+  }
+
+  cancelEdit(): void {
+    this.isEditing = false;
+    this.editedBook = {} as Book;
+  }
+
+  deleteBook(book: Book): void {
+    this.books = this.books.filter(item => item.id !== book.id);
+  }
+
+  get filteredBooks(): Book[] {
+    return this.books.filter(book =>
+      book.title.toLowerCase().includes(this.searchKeyword.toLowerCase())
+    );
+  }
+}
